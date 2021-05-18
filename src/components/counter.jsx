e70b12eb67b5ec916816afe54b072ff3f87cf77e
joe061005 <18233368@life.hkbu.edu.hk>
2021-05-18 14:24:53 +0800

    render class attributes with method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -14,14 +14,24 @@ class Counter extends Component {
     //style= {this.styles}
 
 
+    //const x modify    let can modify
+
+
 
     render() { 
+
         return(
             <div>
-               <span style = {{fontSize: 30, fontWeight:'bold'}} className="badge badge-primary m-2">{this.formatCount()}</span> 
+               <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
                <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         ); 
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){

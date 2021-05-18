8a9de9dc024e00c5f3a6fc8c79c49c0829b2ee8b
joe061005 <18233368@life.hkbu.edu.hk>
2021-05-18 14:06:36 +0800

    setting attribues (css style, class)

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,23 +3,30 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count:0,
-        address:{
-            street: ''
-        }
+       
     };
+
+    styles = {
+       fontSize: 50,
+       fontWeight:'bold'
+    };
+
+    //style= {this.styles}
+
+
 
     render() { 
         return(
             <div>
-               <span>{this.formatCount()}</span>
-               <button>Increment</button>
+               <span style = {{fontSize: 30, fontWeight:'bold'}} className="badge badge-primary m-2">{this.formatCount()}</span> 
+               <button className="btn btn-secondary btn-sm">Increment</button>
             </div>
         ); 
     }
 
     formatCount(){
         const {count} = this.state;
-        return count === 0? <h1>Zero</h1> : count;
+        return count === 0? "Zero" : count;
     }
 }
  

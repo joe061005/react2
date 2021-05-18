5908a68e8859eb1c8a6960755ceabd0dfa7bae5e
joe061005 <18233368@life.hkbu.edu.hk>
2021-05-18 18:38:28 +0800

    Passing Event Arguments onClick = {() => this.handleIncrement({id:1}) important: () = > when passing arguments

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -28,22 +28,22 @@ class Counter extends Component {
         return classes;
     }
 
-    formatCount(){
+    formatCount(){ 
         const {count} = this.state;
         return count === 0? "Zero" : count;
     }
 
-    handleIncrement = () => {
+    handleIncrement = (product) => {
+        console.log(product);
         this.setState({count: this.state.count+1});
     }
-
 
     render() {  
 
         return(
             <div>
                <span className={this.getBadgeClasses()}>{this.formatCount()}</span> 
-               <button onClick= {this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+               <button onClick= {() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         ); 
     }
